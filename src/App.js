858b080c3ff86d02b6c1c3380af875c92d904872
Meets windows <meetsundrani@gmail.com>
2023-12-29 20:04:46 +0530

    changed app and removed app css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CitySelector from './CitySelector';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
 
-function App() {
+const cities = ['Bangalore', 'Chennai', 'Kolkata', 'Delhi', 'Mumbai'];
+
+const App = () => {
+  const [selectedCity, setSelectedCity] = useState('Bangalore');
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/forecast.json?key=fe91e0c84e794547961190823232812&q=${selectedCity}&days=1`
+      );
+      console.log(response);
+    };
+
+    fetchWeatherData();
+  }, [selectedCity]);
+
+  const handleCityChange = (city) => {
+    setSelectedCity(city);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Navbar/>
+      <CitySelector selectedCity={selectedCity} onCityChange={handleCityChange} cities={cities} />
+      <button className="btn btn-primary m-3">
+        Show Graph //Todo
+      </button>
     </div>
   );
-}
+};
 
 export default App;
