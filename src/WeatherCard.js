20651d395260f26c77af61932bdc5d2174b171d6
Meets windows <meetsundrani@gmail.com>
2023-12-30 02:53:44 +0530

    added few fields in weather card

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Spinner from './Spinner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import direction from './direction.svg';
 
-// Handle the case where data is not available yet
+// Handle the case where data is not available yet & blur app
 const WeatherCard = ({ weatherData }) => {
     if (!weatherData.current || !weatherData.forecast || !weatherData.forecast.forecastday[0]) {
         return (
@@ -19,6 +20,9 @@ const WeatherCard = ({ weatherData }) => {
                 <p><FontAwesomeIcon icon="fa-solid fa-temperature-quarter" size="lg" /> Temperature: {current.temp_c}°C</p>
                 <p><FontAwesomeIcon icon="fa-solid fa-temperature-arrow-up" size="lg" /> Highest Temperature: {forecast.forecastday[0].day.maxtemp_c}°C</p>
                 <p><FontAwesomeIcon icon="fa-solid fa-temperature-arrow-down" size="lg" /> Lowest Temperature: {forecast.forecastday[0].day.mintemp_c}°C</p>
+                <p><FontAwesomeIcon icon="fa-solid fa-wind" size="lg" /> Wind: {current.wind_kph} KPH</p>
+                <p><FontAwesomeIcon icon="fa-solid fa-compass" size="lg" /> Wind Direction: {current.wind_dir}</p>
+                <p><img src={current.condition.icon} alt="Current Condition img"/>{current.condition.text}</p>
             </div>
         </div>
     );
