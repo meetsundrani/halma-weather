c47aeece1135b54a0f42d182fac38d98cc113ab2
Meets windows <meetsundrani@gmail.com>
2023-12-30 00:20:03 +0530

    added temp graph component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CitySelector from './CitySelector';
+import WeatherCard from './WeatherCard';
+import TemperatureGraph from './TemperatureGraph';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
 
@@ -8,28 +10,55 @@ const cities = ['Bangalore', 'Chennai', 'Kolkata', 'Delhi', 'Mumbai'];
 
 const App = () => {
   const [selectedCity, setSelectedCity] = useState('Bangalore');
+  const [weatherData, setWeatherData] = useState({});
+  const [showGraph, setShowGraph] = useState(false);
+  const [isLoading, setisLoading] = useState(false);
+  const [cityData, setCityData] = useState({});
 
   useEffect(() => {
     const fetchWeatherData = async () => {
+      setisLoading(true);
       const response = await axios.get(
         `https://api.weatherapi.com/v1/forecast.json?key=fe91e0c84e794547961190823232812&q=${selectedCity}&days=1`
       );
-      console.log(response);
+      setWeatherData(response.data);
+      setisLoading(false);
     };
 
     fetchWeatherData();
   }, [selectedCity]);
 
+  if (isLoading === true) {
+    <div className="spinner-border center" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  }
+
   const handleCityChange = (city) => {
     setSelectedCity(city);
   };
+
+  const handleShowGraph = async () => {
+    const data = {};
+    for (const city of cities) {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/forecast.json?key=fe91e0c84e794547961190823232812&q=${city}&days=1`
+      );
+      data[city] = response.data;
+    }
+    setCityData(data);
+    setShowGraph(true);
+  };
+
   return (
     <div className="container">
-    <Navbar/>
+      <Navbar />
       <CitySelector selectedCity={selectedCity} onCityChange={handleCityChange} cities={cities} />
-      <button className="btn btn-primary m-3">
-        Show Graph //Todo
+      <WeatherCard weatherData={weatherData} />
+      <button className="btn btn-primary m-3" onClick={handleShowGraph}>
+        Show Graph
       </button>
+      {showGraph && <TemperatureGraph cityData={cityData} />}
     </div>
   );
 };
---
+++ src/TemperatureGraph.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    BarElement,
+    Tooltip,
+    Title,
+    LinearScale,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const TemperatureGraph = ({ cityData }) => {
+    const cities = Object.keys(cityData);
+    const temperatures = cities.map((city) => cityData[city].current.temp_c);
+    console.log(temperatures);
+
+    const data = {
+        labels: cities,
+        datasets: [
+            {
+                label: 'Current Temperature',
+                data: temperatures,
+                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#9C27B0'],
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <h2>Temperature Comparison Graph</h2>
+            <Bar data={data} />
+        </div>
+    );
+};
+
+export default TemperatureGraph;
