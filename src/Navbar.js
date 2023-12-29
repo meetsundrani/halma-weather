3909e83e69f9f7f8be3c13156f18b234a1fe232d
Meets windows <meetsundrani@gmail.com>
2023-12-29 14:17:05 +0530

    added navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+return (
+    <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="./">
+                <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+                    Bootstrap
+            </a>
+        </div>
+    </nav>
+);
+}
+
+export default Navbar;
